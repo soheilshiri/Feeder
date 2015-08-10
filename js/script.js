a07bbf650861eaa6c9cf7760e70c6046a3353f07
Soheil Shiri <soheil.shiri@gmail.com>
2015-08-11 00:31:06 +0430

    add item

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // http://feeds.narenji.ir/narenji
-// http://feeds2.feedburner.com/Line25
+// http://
+// feeds2.feedburner.com/Line25
 // http://p30download.com/fa/main/feed/rss.xml
 
 
@@ -9,8 +10,24 @@ var app = angular.module('feeder', [])
 
         $scope.url_nums = 0;
         $scope.URL = "";
-        $scope.newURL = "";
         $scope.sites = [];
+        $scope.results;
+
+
+        $scope.show = function(index) {
+
+            $http({
+                url : $scope.sites[index]['rss'],
+                method : 'GET'
+            })
+                .success(function(data){
+                    $scope.results = data['query']['results']['item'];
+                    console.log($scope.results);
+                })
+                .error(function(error) {
+                    console.error(error);
+                });
+        }
 
         $scope.add_feed = function() {
             $('.small.modal')
@@ -18,13 +35,22 @@ var app = angular.module('feeder', [])
             ;
         }
 
-        //SELECT title    FROM   feednormalizer   WHERE  output="atom_1.0" AND
-        $scope.YQL = function (URL) {
+        // SELECT title FROM feed normalizer WHERE output="atom_1.0" AND
+        $scope.YQL_title = function (URL) {
             $scope.yqlURL = 'https://query.yahooapis.com/v1/public'
                 + '/yql?q=SELECT%20title%0AFROM%20%20%20feednormalizer'
                 + '%0AWHERE%20%20output%3D%22atom_1.0%22%20AND%20url%3D%22'
                 + encodeURIComponent(URL)
                 + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
+            return $scope.yqlURL;
+        }
+
+        $scope.YQL_rss = function (URL) {
+            $scope.yqlURL = "https://query.yahooapis.com/v1/public/"
+                + "yql?q=select%20title%2C%20description%2C%20pubDate%2C%20link%20from%20rss%20where%20url%3D%22"
+                + encodeURIComponent(URL)
+                + "%3Falt%3Drss%26format%3Djson%26diagnostics%3Dtrue%22&format=json"
+                + "&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
             return $scope.yqlURL;
         }
 
@@ -41,12 +67,26 @@ var app = angular.module('feeder', [])
             //    }
             //}
 
-            $scope.newURL = $scope.YQL('http://' + URL);
+            $scope.url_title = $scope.YQL_title('http://' + URL);
+            $scope.url_rss = $scope.YQL_rss('http://' + URL);
+
+
+            $http({
+                url : $scope.url_title,
+                method : 'GET'
+            })
+                .success(function(data){
+                    $scope.results = data['query']['results'];
+                    $scope.sites.push({ url: URL, rss:  $scope.url_rss, name: $scope.results["feed"]["title"]});
+                })
+                .error(function(error) {
+                    console.error(error);
+            });
+
 
             //console.log('http://' + URL);
             //console.log($scope.newURL);
 
-            $scope.sites.push($scope.newURL);
 
 
 
@@ -163,6 +203,7 @@ function makeNewTitle(URL) {
     + '%22&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback='
     return yqlURL;
 }
+
 
 function doChange(DIV){
     
